310b9cb70e104177562a695432cf5cdb3f82e01a
Whocares <yuldashevmrb@gmail.com>
2022-09-22 21:16:18 +0500

    done!

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,27 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+
 
 
 public class FileReader {
-
+    static Profile profile;
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try(BufferedReader scanner = new BufferedReader(new java.io.FileReader(file))) {
+            String name = scanner.readLine();
+            String age = scanner.readLine();
+            String email = scanner.readLine();
+            String phone = scanner.readLine();
+            name = name.substring(name.indexOf(" ")+1);
+            age = age.substring(age.indexOf(" ")+1);
+            email = email.substring(email.indexOf(" ")+1);
+            phone = phone.substring(phone.indexOf(" ")+1);
+            profile = new Profile(name, Integer.valueOf(age), email,  Long.valueOf(phone) );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
